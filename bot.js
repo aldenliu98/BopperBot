d3a6d80a19704ac244ded6315f95c5f2402c8bac
Kelvin Zhang <Kelvin Zhang>
2017-11-14 23:17:07 -0800

    queue update

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ const prefix = config.prefix;
 const discord_token = config.token;
 
 var queue = [];
+var namequeue = [];
 var isPlaying = false;
 var dispatcher = null;
 var voiceChannel = null;
@@ -76,10 +77,25 @@ client.on('message', function(message) {
 			var status = isPlaying? "playing" : "not playing";
 			output += "Status: " + status + "\n";
 			var nums = Math.min(queue.length, 6);
-			addToOutput(nums, 0, function() {
+
+			for( var i = 0; i < namequeue.length; i++){
+
+				if(i === 0){
+					output += "Now Playing" + ": **" + namequeue[i] + "**\n";
+				}
+				else{
+					output += i + ": **" + namequeue[i] + "**\n";
+				}
+
+			}
+
+			message.channel.send(output);
+
+			output = "";
+			/**addToOutput(nums, 0, function() {
 				message.channel.send(output);
 				output = "";
-			});
+			}); **/
 		}
 	}
 
@@ -137,6 +153,7 @@ function commandPlay(message, args) {
 				fetchVideoInfo(id, function(err, videoInfo) {
 					if (err) throw new Error(err);
 					message.channel.send(" added to queue: **" + videoInfo.title + "**");
+					namequeue.push(videoInfo.title);
 				});
 			});
 		} else {
@@ -148,6 +165,7 @@ function commandPlay(message, args) {
 				fetchVideoInfo(id, function(err, videoInfo) {
 					if (err) throw new Error(err);
 					message.channel.send(" now playing: **" + videoInfo.title + "**");
+					namequeue.push(videoInfo.title);
 				});
 			});
 		}
@@ -168,6 +186,7 @@ function playMusic(id, message) {
 		dispatcher = connection.playStream(stream, streamOptions);
 		dispatcher.on('end', function() {
 			queue.shift();
+			namequeue.shift();
 			if (queue.length === 0) {
 				queue = [];
 				isPlaying = false;
